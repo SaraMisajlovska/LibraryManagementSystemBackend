30d76f8753a927b91b2a192b3f9a47e83f1e4e5b
SaraMisajlovska <smisajlovska@gmail.com>
2023-06-17 09:10:09 +0200

    initial app commit

---
+++ src/test/java/mk/ukim/finki/lms/LmsApplicationTests.java
@@ -0,0 +1,12 @@
+package mk.ukim.finki.lms;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
