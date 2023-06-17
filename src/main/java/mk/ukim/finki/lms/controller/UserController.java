704248c22a870d69456d3175452fc02008a6debf
Iva Jadrovska <iva.jadrovska@students.finki.ukim.mk>
2023-06-17 19:05:46 +0200

    function register added

--- src/main/java/mk/ukim/finki/lms/controller/UserController.java
+++ src/main/java/mk/ukim/finki/lms/controller/UserController.java
@@ -2,9 +2,12 @@ package mk.ukim.finki.lms.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import mk.ukim.finki.lms.repository.UserRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 @Slf4j
@@ -29,6 +32,25 @@ public class UserController {
       // User login failed
       return null; // Replace "failure-template" with the appropriate template
     }
+  }
+
+  @PostMapping("/register")
+  public String registerPatron(@RequestParam("email") String email,
+                               @RequestParam("userPassword") String userPassword,
+                               @RequestParam("firstName") String firstName,
+                               @RequestParam("lastName") String lastName,
+                               @RequestParam("dateOfBirth") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateOfBirth,
+                               @RequestParam("address") String address,
+                               @RequestParam("phoneNumber") String phoneNumber,
+                               @RequestParam("cardNumber") Integer cardNumber,
+                               @RequestParam("membershipId") Integer membershipId) {
+
+    // Invoke the register_patron function
+    userRepository.register(email, userPassword, firstName, lastName, dateOfBirth,
+            address, phoneNumber, cardNumber, membershipId);
+
+    // Return a response indicating success
+    return null;
   }
 }
 
--- src/main/java/mk/ukim/finki/lms/repository/UserRepository.java
+++ src/main/java/mk/ukim/finki/lms/repository/UserRepository.java
@@ -3,6 +3,8 @@ package mk.ukim.finki.lms.repository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public class UserRepository {
 
@@ -15,5 +17,14 @@ public class UserRepository {
   public boolean login(String email, String password) {
     String sql = "SELECT login(?, ?)";
     return Boolean.TRUE.equals(jdbcTemplate.queryForObject(sql, Boolean.class, email, password));
+  }
+
+  public void register(String p_email, String p_user_password, String p_first_name,
+                       String p_last_name, LocalDate p_date_of_birth, String p_address,
+                       String p_phone_number, Integer p_card_number, Integer p_membership_id) {
+
+    String sql = "SELECT register_patron(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    jdbcTemplate.update(sql, p_email, p_user_password, p_first_name, p_last_name, p_date_of_birth,
+            p_address, p_phone_number, p_card_number, p_membership_id);
   }
 }
